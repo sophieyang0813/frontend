e74ac109f90e06bbcd31362796521fd59b76eab4
nbiton <naor.biton@gmail.com>
2017-07-07 12:11:14 +0800

    chore: Imported claims collection to server code for initialization

--- server/main.js
+++ server/main.js
@@ -1,6 +1,7 @@
-import { Meteor } from 'meteor/meteor';
-import '../imports/api/tasks.js';
+import { Meteor } from 'meteor/meteor'
+import '../imports/api/tasks'
+import '../imports/api/claims'
 
 Meteor.startup(() => {
   // code to run on server at startup
-});
+})
