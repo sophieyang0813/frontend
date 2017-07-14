0a2137fc24b089c1a493b5fb4b9f8f47f2260c1c
nbiton <naor.biton@gmail.com>
2017-07-14 17:10:43 +0800

    feat: added hidden route for '/demo-claim'

--- imports/ui/routes.jsx
+++ imports/ui/routes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link, Route } from 'react-router-dom'
 import { Dashboard } from './components/dashboard.jsx'
+import Claim from './claim/claim.jsx'
 import App from './app.jsx'
 
 export class Routes extends React.Component {
@@ -8,6 +9,7 @@ export class Routes extends React.Component {
     return (
       <div>
         <Route exact path='/' component={Dashboard} />
+        <Route exact path='/demo-claim' component={Claim} />
         <Route exact path='/todo' component={App} />
         <nav>
           <Link to='/'>Dashboard</Link>
