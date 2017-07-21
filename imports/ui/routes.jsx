5e8ad5776ded4e0529e4710dce8b6f7e0ecd8517
nbiton <naor.biton@gmail.com>
2017-07-21 17:12:20 +0800

    chore: added tachyons css to the

--- imports/ui/routes.jsx
+++ imports/ui/routes.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link, Route } from 'react-router-dom'
 import { Dashboard } from './components/dashboard.jsx'
 import Claim from './claim/claim.jsx'
-import App from './app.jsx'
+import App from './todo/app.jsx'
 
 import AccountsUIWrapper from './accounts-ui-wrapper.jsx'
 
