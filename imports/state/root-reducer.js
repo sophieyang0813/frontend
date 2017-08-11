d4e9eaafe8657569bc843a03649eee940c70303c
nbiton <naor.biton@gmail.com>
2017-08-11 16:41:42 +0800

    chore: integrated react-redux-router into the mix

--- imports/state/root-reducer.js
+++ imports/state/root-reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
+import { routerReducer as router } from 'react-router-redux'
 import hideCompleted from './reducers/hide-completed'
 
 const rootReducer = combineReducers({
-  hideCompleted
+  hideCompleted,
+  router
 })
 
 export default rootReducer
