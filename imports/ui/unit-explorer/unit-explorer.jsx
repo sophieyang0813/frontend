347fa489feae909c6b5c66d60efd2e3708c64369
Sophie Yang <sophieyang0813@gmail.com>
2018-07-13 15:06:02 +0800

    fix: apply fabdescriptor values directly to the button

--- imports/ui/unit-explorer/unit-explorer.jsx
+++ imports/ui/unit-explorer/unit-explorer.jsx
@@ -17,12 +17,6 @@ class UnitExplorer extends Component {
   render () {
     const { isLoading, unitList, dispatch } = this.props
     if (isLoading) return <Preloader />
-    const fabDescriptor =
-      {
-        color: 'var(--bondi-blue)',
-        href: `/unit/new`,
-        icon: 'add'
-      }
 
     return (
       <div className='flex flex-column flex-grow full-height'>
@@ -62,10 +56,10 @@ class UnitExplorer extends Component {
             </div>
             <div className='absolute bottom-2 right-2'>
               <FloatingActionButton
-                backgroundColor={fabDescriptor.color}
-                onClick={() => dispatch(push(fabDescriptor.href))}
+                className='bondi-blue'
+                onClick={() => dispatch(push(`/unit/new`))}
               >
-                <FontIcon className='material-icons'>{fabDescriptor.icon}</FontIcon>
+                <FontIcon className='material-icons'>add</FontIcon>
               </FloatingActionButton>
             </div>
           </div>
