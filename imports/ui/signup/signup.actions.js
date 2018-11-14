f3985782cd5f89627785076acd4b7add3c236597
Sophie Yang <sophieyang0813@gmail.com>
2018-11-14 12:13:05 +0800

    feat: loading icon during user creation

--- imports/ui/signup/signup.actions.js
+++ imports/ui/signup/signup.actions.js
@@ -2,10 +2,14 @@
 import { Accounts } from 'meteor/accounts-base'
 import routerRedux from 'react-router-redux'
 export const SIGNUP_ERROR = 'signup_error'
+export const SIGNUP_IN_PROGRESS = 'signup_in_progress'
 
 export function submitSignupInfo (info) {
   const { push } = routerRedux
   return (dispatch) => {
+    dispatch({
+      type: SIGNUP_IN_PROGRESS
+    })
     Accounts.createUser({
       email: info.emailAddress,
       password: info.password,
